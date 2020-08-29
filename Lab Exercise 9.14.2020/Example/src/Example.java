4337415de1828d9dd746a32111bc9210cbe2b2a1
nmessa <nmessa@sau16.org>
2020-08-29 18:16:15 -0400

    Add files via upload

---
+++ Lab Exercise 9.14.2020/Example/src/Example.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)Example.java
+ *
+ * Example application
+ *
+ * @Author: 
+ * @version 1.00 2020/9/14
+ */
+
+public class Example {
+
+    public static void main(String[] args) {
+
+    	//Add code here
+    	
+    }
+}
+
+//Output
+//Hours Worked: 40
+//pay Amount  : 400.0
+//tax Amount  : 40.0
