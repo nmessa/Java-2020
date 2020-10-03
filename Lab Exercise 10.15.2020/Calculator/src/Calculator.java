31a6dd6d63ee8d3b71cb135749e5762f097b7ca6
nmessa <nmessa@sau16.org>
2020-10-03 15:04:36 -0400

    Add files via upload

---
+++ Lab Exercise 10.15.2020/Calculator/src/Calculator.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Calculator.java
+ *
+ * Calculator application
+ *
+ * @author 
+ * @version 1.00 10/15/2020
+ */
+ 
+public class Calculator {
+    
+    public static void main(String[] args) {
+    	
+    	JCalculator calc = new JCalculator();
+    }
+}
