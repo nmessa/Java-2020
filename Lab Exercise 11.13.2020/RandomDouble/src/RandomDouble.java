0cf130ed38c14a171c5378df22fbe7e12b99c6ea
nmessa <nmessa@sau16.org>
2020-11-06 14:06:50 -0500

    Add files via upload

---
+++ Lab Exercise 11.13.2020/RandomDouble/src/RandomDouble.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)RandomDouble.java
+ *
+ * RandomDouble application
+ *
+ * @Author: 
+ * @version 1.00 2020/11/13
+ */
+import java.util.*;
+
+public class RandomDouble {
+    
+    public static void main(String[] args) {
+    	//Add code here
+    	
+    	
+    }
+}
---
+++ Lab Exercise 11.13.2020/RandomInt/src/RandomInt.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)RandomInt.java
+ *
+ * RandomInt application
+ *
+ * @Author: 
+ * @version 1.00 2020/11/13
+ */
+import java.util.*;
+
+public class RandomInt {
+    
+    public static void main(String[] args) {
+    	//Add code here
+    	
+    	
+    }
+}
+
+
