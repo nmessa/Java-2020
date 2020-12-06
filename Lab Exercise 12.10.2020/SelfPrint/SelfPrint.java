faf7bcbd8544f528eec0b4b667ec03a2eb3be456
nmessa <nmessa@sau16.org>
2020-12-06 13:26:43 -0500

    Add files via upload

---
+++ Lab Exercise 12.10.2020/SelfPrint/SelfPrint.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)SelfPrint.java
+ *
+ * SelfPrint application
+ *
+ * @Author:  
+ * @version 1.00 2020/12/10
+ */
+import java.util.*;
+import java.io.*;
+
+public class SelfPrint {
+    
+    public static void main(String[] args) throws IOException{
+        //Add code here
+		
+    }
+}
